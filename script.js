43e1be7974f9b1a9cede45aca7ba66b0e1ad8e40
Matt <magin3191@gmail.com>
2017-10-13 11:30:07 -0600

    gradient added

--- script.js
+++ script.js
@@ -20,8 +20,28 @@ for(var i=0; i<81;i++){
   tile.style.float='left';
   document.body.prepend(tile);
 
-
-
-
-
+  if(i%2==0){
+  tile.style.backgroundColor=getRandomColor();
 }
+
+  else{
+    tile.style.backgroundColor=getRandomColor();
+  }
+
+ var body = document.getElementsByTagName('body')[0];
+ body.style.backgroundImage = `linear-gradient(12deg, blue, green)`;
+ body.style.opacity = 0.2;
+ body.appendChild(tile);
+
+tile.addEventListener("click", function(tile){
+
+tile.target.style.backgroundColor=getRandomColor()
+
+})
+}
+
+// function colorChange(){
+// tile.style.backgroundColor=getRandomColor()
+//
+//
+// }
