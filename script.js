6960ae814a80e40d5b1aba1b900cf63903c20fa0
Matt <magin3191@gmail.com>
2017-10-13 10:27:15 -0600

    added random colors

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 
 
-
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 
 
 
 
 for(var i=0; i<81;i++){
-var tile=document.createElement('div');
-tile.style.width='11.1%';
-tile.style.paddingBottom='11.1%';
-tile.style.float='left';
-document.body.prepend(tile);
+  var tile=document.createElement('div');
+  tile.style.width='11.1%';
+  tile.style.paddingBottom='11.1%';
+  tile.style.float='left';
+  document.body.prepend(tile);
 
 
 
