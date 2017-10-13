a46bfac8e7aac4a64f7d3f9decfacb2397bbfa03
Matt <magin3191@gmail.com>
2017-10-13 10:21:50 -0600

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Your JS goes here
+
+
+
+
+
+
+
+
+for(var i=0; i<81;i++){
+var tile=document.createElement('div');
+tile.style.width='11.1%';
+tile.style.paddingBottom='11.1%';
+tile.style.float='left';
+document.body.prepend(tile);
+
+
+
+
+
+}
